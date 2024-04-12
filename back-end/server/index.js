351b69d2184f2d044895ea06bbde8f8c20bac66f
JuanRincon15 <jrincon@falabella.com>
2024-04-12 11:00:52 -0500

    guardar chats por sesion

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -10,6 +10,16 @@ const server = app.listen(3000, () => {
     console.log('Servidor Express escuchando en el puerto 3000');
 });
 
+const {openConnection} = require('../base de datos/conectdb');
+const db = openConnection('base de datos/bd.sqlite3')
+const sessionId = 'session_' + Math.random().toString(36).substr(2, 9);
+console.log(sessionId);
+
+sql=`INSERT INTO sesiones (nombre) VALUES (?);`;
+db.run(sql,[sessionId],(err)=>{
+    if (err) return console.error(err.message);
+});
+
 const io = new Server(server, { cors: { origin: 'http://localhost:4200' } });
 
 io.on('connection', (socket) => {
@@ -18,6 +28,11 @@ io.on('connection', (socket) => {
         console.log(data);
         const segmento = data.split(':');
         socket.broadcast.emit('received', { data: segmento[1], mensajero: segmento[0],rol:segmento[2] });
+        
+        sql=`INSERT INTO mensajes (id_sesion, nombre_usuario, mensaje) VALUES (?, ?, ?);`;
+        db.run(sql,[sessionId,segmento[0],segmento[1]],(err)=>{
+            if (err) return console.error(err.message);
+        });
     });
 });
 
